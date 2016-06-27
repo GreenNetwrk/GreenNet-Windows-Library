a21ab36c79b4d47e402ca8d3f41d953f69aebf4a
GreenCommunications <alexander.m.rankin@gmail.com>
2016-06-26 21:03:33 -0400

    Added code

---
+++ greennetlibrary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+public class greennetHost
+{
+    private string httpGET(string sURL)
+    {
+        WebRequest request = WebRequest.Create(sURL);
+
+        WebResponse response = request.GetResponse();
+
+        Stream dataStream = response.GetResponseStream();
+
+        StreamReader reader = new StreamReader(dataStream);
+
+        string responseFromServer = reader.ReadToEnd();
+
+        return responseFromServer;
+    }
+	public string pullRequests()
+	{
+        
+	}
+}
